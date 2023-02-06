c6d3710793b9f5022295c6a51afb9c73a8b75f05
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-02-06 07:10:30 -0300

    feat: configure theme colors & font Family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif",
+      },
+
+      colors: {
+
+        gray: {
+          100: '#E1E1E6',
+        },
+
+        green: {
+          200: "#00B37E",
+          500: "#07847E"
+        },
+
+        purple: {
+          100: '#282843',
+          500: "#633BBC",
+          800: "#282843",
+          900: "#1A1924"
+        }
+      },
+    },
   },
   plugins: [],
 };
