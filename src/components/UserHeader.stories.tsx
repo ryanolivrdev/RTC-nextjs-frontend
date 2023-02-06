b56114e093bbbb0c40877a691b18d984f7a28aef
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-02-06 07:19:17 -0300

    feat: storybook header user

---
+++ src/components/UserHeader.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import type { UserHeaderProps } from './UserHeader';
+import { UserHeader } from './UserHeader'
+
+export default {
+  title: 'Components/Header/User',
+  component: UserHeader,
+  args: {
+    name: 'Ana Caetano',
+    status: 'online',
+  },
+  argTypes: {
+    status: {
+      options: ['online', 'away', 'offline'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<UserHeaderProps>
+
+export const Default: StoryObj<UserHeaderProps> = {}
